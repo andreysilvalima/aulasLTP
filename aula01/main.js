219d5ad990ad71b572276291889e5a85d2abb210
andreysilvalima <andreysilma1609@gmail.com>
2023-08-24 23:41:40 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,19 @@
+//comentarios são feitos com ''//''
+//console.log é usado para mostrar uma mensagem para o usuario do console
+console.log("hello");
+//alert emite uma mensagem de alerta ao usuario
+alert('Cuidado manin, pode ser que tenha ou não hackers de olho em você. E pode ser ou não que seja eu');
+
+// para declarar variaveis no js, podemos usar 'var', 'let' ou 'const'
+
+const pi = 3.14;
+
+//sempre que usar const para declarar uma variavel, o valor dela não pode ser alterado!!
+
+// Let é uma variavel de escopo local, enquando Var é global
+
+var nome= 'xuliano';
+
+nome= 'xuliana';
+alert(nome);
+
